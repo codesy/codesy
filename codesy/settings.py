d1580d5e8efd3d18e38324b6fd77ab54fddb8749
groovecoder <luke.crouch@gmail.com>
2014-04-10 22:45:58 -0500

    new-style settings

--- codesy/settings.py
+++ codesy/settings.py
@@ -29,16 +29,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+)
 
+THIRD_PARTY_APPS = (
     'south',
 )
+
+LOCAL_APPS = (
+)
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
