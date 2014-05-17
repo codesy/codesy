7db4e68e397c8ebdbb9c4fef62a0db88e82b18aa
groovecoder <luke.crouch@gmail.com>
2014-05-17 00:08:32 -0500

    use SendGrid for emails

--- codesy/settings.py
+++ codesy/settings.py
@@ -80,7 +80,7 @@ AUTHENTICATION_BACKENDS = (
 
 # auth and allauth settings
 LOGIN_REDIRECT_URL = '/'
-SOCIALACCOUNT_QUERY_EMAIL = False
+SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_PROVIDERS = {
     'github': {
         'SCOPE': ['email',],
@@ -140,3 +140,9 @@ STATICFILES_DIRS = (
 )
 
 SITE_ID = 1
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
