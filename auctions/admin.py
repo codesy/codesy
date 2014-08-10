90861ea29a7c36c5d3857ad751c4a0c30a361894
groovecoder <luke.crouch@gmail.com>
2014-08-09 20:49:50 -0500

    fix profiles and PUT balanced tokens to profile

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .models import Bid, Profile
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Bid)
-admin.site.register(Profile)
--- auctions/models.py
+++ auctions/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User)
     balanced_card_href = models.CharField(max_length=100)
     balanced_bank_account_href = models.CharField(max_length=100)
 
@@ -13,3 +14,9 @@ class Bid(models.Model):
     url = models.URLField()
     ask = models.DecimalField(max_digits=6, decimal_places=2)
     offer = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+@receiver(models.signals.post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+        if created and not kwargs.get('raw', False):
+            p, created = Profile.objects.get_or_create(user=instance)
