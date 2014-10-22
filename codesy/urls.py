48d1956f4ec5abee943b3c1cad8685a267efe5e6
groovecoder <luke.crouch@gmail.com>
2014-10-22 17:11:28 -0500

    fixes #30 - add User API and PATCH with CC token

--- codesy/urls.py
+++ codesy/urls.py
@@ -2,8 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from rest_framework import routers
+
+from . import views
+
 admin.site.site_header = u'Codesy administration'
 admin.site.site_title = u'Codesy site admin'
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = patterns('',
     # Special view for whats-deployed app
@@ -27,6 +34,9 @@ urlpatterns = patterns('',
     # API docs
     url(r'^docs/', include('rest_framework_swagger.urls')),
 
-    #auctions API
-    url(r'^', include('auctions.urls'))
+    # auctions API
+    url(r'^', include('auctions.urls')),
+
+    # user API
+    url(r'^', include(router.urls))
 )
