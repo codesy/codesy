fbfbb584751548e1f07d5427cfa740e9a9a7c1d1
groovecoder <luke.crouch@gmail.com>
2014-10-26 17:49:49 -0500

    fixes #21 - save email from GitHub API to user

--- codesy/base/models.py
+++ codesy/base/models.py
@@ -1,9 +1,13 @@
+import requests
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.dispatch import receiver
 
-from allauth.socialaccount.signals import social_account_added
 from allauth.account.signals import user_signed_up
+
+
+EMAIL_URL = 'https://api.github.com/user/emails'
 
 
 class User(AbstractUser):
@@ -13,10 +17,13 @@ class User(AbstractUser):
     USERNAME_FIELD = 'username'
 
 
-@receiver(social_account_added)
-def add_email_from_account(sender, request, sociallogin, **kwargs):
-    import ipdb; ipdb.set_trace()
-
 @receiver(user_signed_up)
 def add_email_from_signup(sender, request, user, **kwargs):
-    import ipdb; ipdb.set_trace()
+    params = {'access_token': kwargs['sociallogin'].token}
+    email_data = requests.get(EMAIL_URL, params=params).json()
+    for email_address in email_data:
+        if (email_address.get('verified', False) and
+                email_address.get('primary', False)):
+            user.email = email_address['email']
+            user.save()
+            break
