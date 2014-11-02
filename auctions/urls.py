a5f4e60b05772de7aeb464827b7a1b0bbcaa7f07
groovecoder <luke.crouch@gmail.com>
2014-11-02 13:24:11 -0600

    fix #35 - fix bid widget for PUT and POST

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,12 +8,12 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'bids', views.BidViewSet)
 
-single_bid_patterns = patterns('',
+bid_by_url_patterns = patterns('',
     url(r'^bid/', views.GetBid.as_view()),
 )
 
 urlpatterns = patterns('',
-    url(r'^', include(single_bid_patterns, namespace="single_bid")),
+    url(r'^', include(bid_by_url_patterns, namespace="bid-by-url")),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
