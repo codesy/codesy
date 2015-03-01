210ca104c70a47a00d7515238f3e89dd1a9de421
groovecoder <luke.crouch@gmail.com>
2015-03-01 13:42:10 -0600

    for #131 - add Claim to admin site

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Bid, Issue
+from .models import Bid, Issue, Claim
 
 admin.site.register(Bid)
 admin.site.register(Issue)
+admin.site.register(Claim)
