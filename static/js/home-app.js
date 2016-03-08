7621233698e7aa8cbee56f5f36f7df52c3514c5d
jdungan <johnadungan@gmail.com>
2016-03-07 20:12:26 -0600

    fix #252 added CSRF token to ajax call

--- static/js/home-app.js
+++ static/js/home-app.js
@@ -4,42 +4,44 @@ $(window).load(function () {
   Stripe.setPublishableKey('pk_test_NBTdQSXW5HcsEVNgzOZmmZng');
 
   $('#refreshing').hide();
-
-  // if ($("#api_token_pass").length > 0) {
-  //   codesy.user.token = $("#api_token_pass").val();
-  // }
   
   if ($("#codesy_user_id").length > 0) {
     codesy.user.id = $("#codesy_user_id").val();
   }
 
-  function handleResponse(status, response) {
-    if (response.error) {
-      console.error("Stripe failed to tokenize");
-      $('#payment-errors').text(response.error.message);
-      $('#refreshing').hide();
-    } else {
-      var token = response.id;
-      $.ajax({
-        method: "PATCH",
-        url: "/users/"+codesy.user.id + "/",
-        beforeSend: function(xhr, settings) {
-          xhr.setRequestHeader("Authorization","Token "+ codesy.user.token);
-        },
-        data: {
-          stripe_card_token: token
-        },
-        success: function(data, status, jqXHR) {
-          console.log("Updated user.");
-          document.location.reload();
-          $('#refreshing').hide();
-        },
-        error: function(err) {
-          console.error("Error updating user.");
-          $('#refreshing').hide();
-        },
-      });
-    }
+  stripeResponse = function (csrf_token) {
+      this.csrf_token = csrf_token
+      return (function(_this){
+          return function (status, response) {
+              if (response.error) {
+                console.error("Stripe failed to tokenize");
+                $('#payment-errors').text(response.error.message);
+                $('#refreshing').hide();
+              } else {
+                var token = response.id;
+                $.ajax({
+                  method: "PATCH",
+                  url: "/users/"+codesy.user.id + "/",
+                  beforeSend: function(xhr, settings) {
+                    xhr.setRequestHeader('X-CSRFToken', _this.csrf_token);
+                  },
+                  data: {
+                    stripe_card_token: token
+                  },
+                  success: function(data, status, jqXHR) {
+                    console.log("Updated user.");
+                    document.location.reload();
+                    $('#refreshing').hide();
+                  },
+                  error: function(err) {
+                    console.error("Error updating user.");
+                    console.error(err);
+                    $('#refreshing').hide();
+                  },
+                });
+              }
+          }
+    })(this)
   }
 
   $('a#cc-show').click(function (e) {
@@ -55,9 +57,11 @@ $(window).load(function () {
       number: $('#cc-number').val(),
       exp_month: $('#cc-ex-month').val(),
       exp_year: $('#cc-ex-year').val(),
-      cvc: $('#ex-cvv').val()
+      cvc: $('#cvc').val()
     };
 
+    handleResponse = new stripeResponse($('form [name="csrfmiddlewaretoken"]').val())
+    
     // Create credit card
     Stripe.card.createToken(payload, handleResponse);
   });
