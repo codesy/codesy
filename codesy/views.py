79facabc259e6a5591239740f22d9e5618b5b455
jdungan <johnadungan@gmail.com>
2016-05-29 15:26:22 -0500

    add stripe key to context, update templates to use it

--- codesy/views.py
+++ codesy/views.py
@@ -2,15 +2,17 @@ from django.views.generic import TemplateView
 from django.conf import settings
 
 
-def add_cc_debug(ctx):
+def add_stripe_context(ctx):
+    stripe_context = {}
+    stripe_context['STRIPE_PUBLIC_KEY'] = settings.STRIPE_PUBLIC_KEY
     if settings.DEBUG:
-        cc_debug = {
+        stripe_context['test_cc'] = {
             'cc_number': '4111111111111111',
             'cc_ex_month': '01',
             'cc_ex_year': '2020',
             'cvc': '123',
         }
-        ctx['cc_debug'] = cc_debug
+    ctx['stripe_context'] = stripe_context
     return ctx
 
 
@@ -19,7 +21,7 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(Home, self).get_context_data(**kwargs)
-        ctx = add_cc_debug(ctx)
+        ctx = add_stripe_context(ctx)
         browser = 'unknown'
         if (hasattr(self.request, 'META') and
                 'HTTP_USER_AGENT' in self.request.META):
@@ -44,7 +46,7 @@ class CardInfo(TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(CardInfo, self).get_context_data(**kwargs)
-        ctx = add_cc_debug(ctx)
+        ctx = add_stripe_context(ctx)
         return ctx
 
 
