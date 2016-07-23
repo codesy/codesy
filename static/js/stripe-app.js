e35dc1522781d13b8f7c0cfeab6323f0dd53ea51
jdungan <johnadungan@gmail.com>
2016-07-23 13:53:45 -0500

    reorganize stripe templates

--- static/js/stripe-app.js
+++ static/js/stripe-app.js
@@ -1,11 +1,6 @@
 $(window).load(function () {
     Stripe.setPublishableKey($('#codesy-html').data('stripe_key'));
 
-    var codesy = {user:{}}
-    if ($("#codesy_user_id").length > 0) {
-    codesy.user.id = $("#codesy_user_id").val();
-    }
-
     stripeResponse = function (csrf_token) {
         this.csrf_token = csrf_token
         return (function(_this){
@@ -15,16 +10,13 @@ $(window).load(function () {
                     $('#payment-errors').text(response.error.message);
                     document.location.reload();
                 } else {
-                    var token = response.id;
                     $.ajax({
                         method: "PATCH",
-                        url: "/users/"+codesy.user.id + "/",
+                        url: "/users/update/",
                         beforeSend: function(xhr, settings) {
                             xhr.setRequestHeader('X-CSRFToken', _this.csrf_token);
                         },
-                        data: {
-                        stripe_cc_token: token
-                        },
+                        data: {['stripe_'+response.type]: response.id},
                         success: function(data, status, jqXHR) {
                             console.log("Updated user.");
                         },
@@ -41,18 +33,11 @@ $(window).load(function () {
         })(this)
     }
 
-    $('#cc-submit').click(function (e) {
+    $('#stripe-submit').click(function (e) {
         e.preventDefault();
         $('#cc-submit').text('Authorizing ... ');
-        var payload = {
-            number: $('#cc-number').val(),
-            exp_month: $('#cc-ex-month').val(),
-            exp_year: $('#cc-ex-year').val(),
-            cvc: $('#cvc').val()
-        };
         handleResponse = new stripeResponse($('form input[name="csrfmiddlewaretoken"]').val())
-        // Create credit card
-        Stripe.card.createToken(payload, handleResponse);
+        Stripe.bankAccount.createToken($('#stripe-form'), handleResponse);
     });
 
 })
