a9d0de933f735e4e2f2d76bebbb9a6fe9b7bf394
jdungan <johnadungan@gmail.com>
2016-08-12 22:08:52 -0500

    misc PR comments

--- payments/urls.py
+++ payments/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^events', views.StripeHookView.as_view(), name='events'),
-    url(r'^terms', views.AcceptTermsView.as_view(), name='terms'),
-    url(r'^identity', views.VerifyIdentityView.as_view(), name='identity'),
+    url(r'^events$', views.StripeHookView.as_view(), name='events'),
+    url(r'^terms$', views.AcceptTermsView.as_view(), name='terms'),
+    url(r'^identity$', views.VerifyIdentityView.as_view(), name='identity'),
     url(r'^bank$', views.BankAccountView.as_view(), name='bank'),
     url(r'^card$', views.CreditCardView.as_view(), name='card'),
 
