ddb071eb6b7e7e1569004c0d0ba1c12b45b9502e
groovecoder <luke.crouch@gmail.com>
2018-04-12 22:22:32 -0500

    always redirect to www

--- codesy/base/middleware.py
+++ codesy/base/middleware.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 
 class AuthChangedMiddleware(object):
@@ -18,3 +20,24 @@ class AuthChangedMiddleware(object):
                 response['x-codesy-auth-changed'] = 'true'
 
         return response
+
+
+class AlwaysWWW(object):
+    """
+    Always redirect users to www.codesy.io so they don't get HSTS pinned to
+    https://codesy.io which breaks GitHub auth
+    """
+    def process_request(self, request):
+        if (
+            settings.DEBUG or
+            request.get_host() == "testserver" or
+            request.get_host().startswith("codesy-stage")
+        ):
+            return
+        if not request.get_host().startswith("www"):
+            return HttpResponseRedirect(
+                request.scheme +
+                '://www.' +
+                request.get_host() +
+                request.get_full_path()
+            )
